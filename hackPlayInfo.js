3056f1bc0552a2463a6b7d0d07adbd6f5083365f
0rango <j.defony@gmail.com>
2015-01-02 18:03:31 +0800

    Update hackPlayInfo.js

--- hackPlayInfo.js
+++ hackPlayInfo.js
@@ -1,20 +1,16 @@
-$('body').ready(function(){
-		$('body').append("<input type=\"hidden\" id=\"songTitle\" \/>");
-		$('body').append("<input type=\"hidden\" id=\"songArtist\" \/>");
-});
-window.extStatusHandler = function(o){
+window.extStatusHandler = function(o) {
   hackPlayInfo(o);
 }
 
-function hackPlayInfo(_o){
+function hackPlayInfo(_o) {
   //alert(_o);
-  var o = eval('('+_o+')');
-  
-  if(o.type == 'start'){
+  var o = eval('(' + _o + ')');
+
+  if (o.type == 'start') {
     var s = o.song;
     //alert(s.artist+s.title);
     $('#songTitle').val(s.title);
     $('#songArtist').val(s.artist);
-    getSongInfoJson(s.title,s.artist);
+    getSongInfoJson(s.title, s.artist);
   }
 }
